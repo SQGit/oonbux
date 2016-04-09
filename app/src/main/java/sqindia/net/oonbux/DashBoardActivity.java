d6d58b1082f62abfd4d35d4a68ee3e54e772dbda
Salman <salman@sqindia.net>
2016-04-09 16:56:20 +0530

    4/9 commit

--- app/src/main/java/sqindia/net/oonbux/DashBoardActivity.java
+++ app/src/main/java/sqindia/net/oonbux/DashBoardActivity.java
@@ -1,32 +1,31 @@
 package sqindia.net.oonbux;
 
+import android.app.FragmentManager;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TabHost;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
-/*salman khan kha edit*/
+import com.rey.material.widget.Button;
 
-public class DashBoardActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
-    TabHost tabHost;
 
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
+
+public class DashBoardActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    TextView txt1, txt2, txt3, txt4, txt5, txt6, txt7, txt8, txt9, txt10, txt11;
+    Button btn1, btn2, btn3,dbtn1,dbtn2,dbtn3;
+    com.rey.material.widget.TextView htxt1, htxt2,header;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_navigation);
+        setContentView(R.layout.activity_dashboard);
 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -39,79 +38,132 @@ public class DashBoardActivity extends AppCompatActivity implements NavigationVi
         navigationView.setNavigationItemSelectedListener(this);
 
 
-       /* tabHost = (TabHost) findViewById(R.id.tabhost);
-        TabHost.TabSpec tab1 = tabHost.newTabSpec("tab1");
-        TabHost.TabSpec tab2 = tabHost.newTabSpec("tab2");
-       // TabHost.TabSpec tab3 = tabHost.newTabSpec("tab3");
+        header = (com.rey.material.widget.TextView) findViewById(R.id.header);
+        htxt1 = (com.rey.material.widget.TextView) findViewById(R.id.htxt1);
+        htxt2 = (com.rey.material.widget.TextView) findViewById(R.id.htxt2);
+        txt1 = (TextView) findViewById(R.id.txt1);
+        txt2 = (TextView) findViewById(R.id.txt2);
+        txt3 = (TextView) findViewById(R.id.txt3);
+        txt4 = (TextView) findViewById(R.id.txt4);
+        txt5 = (TextView) findViewById(R.id.txt5);
+        txt6 = (TextView) findViewById(R.id.txt6);
+        txt7 = (TextView) findViewById(R.id.txt7);
+        txt8 = (TextView) findViewById(R.id.txt8);
+        txt9 = (TextView) findViewById(R.id.txt9);
+        txt10 = (TextView) findViewById(R.id.txt10);
+        txt11 = (TextView) findViewById(R.id.txt11);
+
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+
+        dbtn1 = (Button) findViewById(R.id.dbtn1);
+        dbtn2 = (Button) findViewById(R.id.dbtn2);
+        dbtn3 = (Button) findViewById(R.id.dbtn3);
+
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/prox.otf");
+
+        header.setTypeface(tf);
+        htxt1.setTypeface(tf);
+        htxt2.setTypeface(tf);
+        txt1.setTypeface(tf);
+        txt2.setTypeface(tf);
+        txt3.setTypeface(tf);
+        txt4.setTypeface(tf);
+        txt5.setTypeface(tf);
+        txt6.setTypeface(tf);
+        txt7.setTypeface(tf);
+        txt8.setTypeface(tf);
+        txt9.setTypeface(tf);
+        txt10.setTypeface(tf);
+        txt11.setTypeface(tf);
+        btn1.setTypeface(tf);
+        btn2.setTypeface(tf);
+        btn3.setTypeface(tf);
+        dbtn1.setTypeface(tf);
+        dbtn2.setTypeface(tf);
+        dbtn3.setTypeface(tf);
+
+        First_Fragment fragment = new First_Fragment();
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.frame_container, fragment).commit();
+
+        dbtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dbtn1.setBackgroundColor(getResources().getColor(R.color.tab_brown));
+                dbtn2.setBackgroundColor(getResources().getColor(R.color.tab_default));
+                dbtn3.setBackgroundColor(getResources().getColor(R.color.tab_default));
+
+                First_Fragment fragment = new First_Fragment();
+                FragmentManager fragmentManager = getFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.frame_container, fragment).commit();
+                //Toast.makeText(getApplicationContext(), "dbtn", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        dbtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dbtn1.setBackgroundColor(getResources().getColor(R.color.tab_default));
+                dbtn2.setBackgroundColor(getResources().getColor(R.color.tab_brown));
+                dbtn3.setBackgroundColor(getResources().getColor(R.color.tab_default));
+
+               // Toast.makeText(getApplicationContext(), "dbtn", Toast.LENGTH_LONG).show();
+                Second_Fragment fragment = new Second_Fragment();
+                FragmentManager fragmentManager = getFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.frame_container, fragment).commit();
+
+            }
+        });
 
 
-        tab1.setIndicator("ship your package abroad");
-        Intent igom = new Intent(DashBoardActivity.this, DashboardTab1Activity.class);
-        tab1.setContent(igom);
+        dbtn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        tab2.setIndicator("Deliver your package");
-        Intent igo = new Intent(DashBoardActivity.this, Tab2Activity.class);
-        tab2.setContent(igo);
+                dbtn1.setBackgroundColor(getResources().getColor(R.color.tab_default));
+                dbtn2.setBackgroundColor(getResources().getColor(R.color.tab_default));
+                dbtn3.setBackgroundColor(getResources().getColor(R.color.tab_brown));
 
-        //tab3.setIndicator("Shop Online with one address");
-       // Intent igo1 = new Intent(DashBoardActivity.this, Tab3Activity.class);
-        //tab3.setContent(igo1);
+               // Toast.makeText(getApplicationContext(), "dbtn", Toast.LENGTH_LONG).show();
+                Second_Fragment fragment = new Second_Fragment();
+                FragmentManager fragmentManager = getFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.frame_container, fragment).commit();
 
-        tabHost.addTab(tab1);
-       tabHost.addTab(tab2);
-       // tabHost.addTab(tab3);*/
+            }
+        });
 
 
-        viewPager = (ViewPager) findViewById(R.id.viewpager);
-        setupViewPager(viewPager);
 
-        tabLayout = (TabLayout) findViewById(R.id.tabs);
-//        tabLayout.setupWithViewPager(viewPager);
-        //tabLayout.setupWithViewPager(viewPager);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"button worked",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
+
     }
 
 
-    private void setupViewPager(ViewPager viewPager) {
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new Tab2Activity(), "ONE");
-        adapter.addFragment(new Tab3Activity(), "TWO");
-        // adapter.addFragment(new ThreeFragment(), "THREE");
-        viewPager.setAdapter(adapter);
-    }
+
 
     @Override
     public boolean onNavigationItemSelected(MenuItem menuItem) {
         return false;
     }
 
-    class ViewPagerAdapter extends FragmentPagerAdapter {
-        private final List<Fragment> mFragmentList = new ArrayList<>();
-        private final List<String> mFragmentTitleList = new ArrayList<>();
-
-        public ViewPagerAdapter(FragmentManager manager) {
-            super(manager);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return mFragmentList.get(position);
-        }
-
-        @Override
-        public int getCount() {
-            return mFragmentList.size();
-        }
-
-        public void addFragment(Fragment fragment, String title) {
-            mFragmentList.add(fragment);
-            mFragmentTitleList.add(title);
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return mFragmentTitleList.get(position);
-        }
-    }
 
 
 }
